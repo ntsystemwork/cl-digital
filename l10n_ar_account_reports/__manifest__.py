0b7a966262d09634745efd7b0a22199743a582c4
jobiols <jorge.obiols@gmail.com>
2019-10-25 19:10:41 -0300

    [FIX] add l10n_ar_account_report

---
+++ l10n_ar_account_reports/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Reportes Argentinos de Contabilidad',
+    'summary': 'View and create reports',
+    'category': 'Accounting',
+    'description': """
+Reportes Argentinos de Contabilidad
+===================================
+    """,
+    'depends': ['account'],
+    'data': [
+#        'security/ir.model.access.csv',
+#        'data/account_financial_report_data.xml',
+#        'views/account_report_view.xml',
+#        'views/report_financial.xml',
+#        'views/search_template_view.xml',
+#        'views/report_followup.xml',
+#        'views/partner_view.xml',
+#        'views/account_journal_dashboard_view.xml',
+#        'views/res_config_settings_views.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+    'license': 'AGPL-3',
+}
