7e7e150e11a64fd4c653e933266fab8e2aaf6977
jobiols <jorge.obiols@gmail.com>
2019-10-25 21:51:34 -0300

    [FIX] esqueleto del reporte

--- l10n_ar_account_reports/__manifest__.py
+++ l10n_ar_account_reports/__manifest__.py
@@ -10,15 +10,7 @@ Reportes Argentinos de Contabilidad
     """,
     'depends': ['account'],
     'data': [
-#        'security/ir.model.access.csv',
-#        'data/account_financial_report_data.xml',
-#        'views/account_report_view.xml',
-#        'views/report_financial.xml',
-#        'views/search_template_view.xml',
-#        'views/report_followup.xml',
-#        'views/partner_view.xml',
-#        'views/account_journal_dashboard_view.xml',
-#        'views/res_config_settings_views.xml'
+        'data/account_financial_report_data.xml',
     ],
     'auto_install': False,
     'installable': True,
