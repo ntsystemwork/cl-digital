336d3e055666b4f96642e66483a4c3e0294a6ff9
Guido <guidoomuchiutti@gmail.com>
2023-10-06 09:41:41 -0300

    CFE transmision  mipyme agregado en factura

---
+++ l10n_ar_afipws_fe_fce_mipyme/l10n_ar_afipws_fe_fce_mipyme/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "l10n_ar_afip_fce_transmission ",
+
+    'summary': u"",
+
+    'description': u"""
+            
+    """,
+
+    'author': "Ingenio Solutions",
+    'website': "http://ingeniosolutions.com.ar",
+    'category': 'Specific Industry Applications',
+    'version': '1.0.0',
+    'depends': ['l10n_ar_afipws_fe'],
+    'data': [
+        'views/invoice_inherit.xml'
+
+
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False
+
+}
